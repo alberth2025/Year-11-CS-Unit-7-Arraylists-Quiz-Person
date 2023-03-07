987031ab7d2d197662e65fe826f3895c883180f0
alberth2025 <alberth2025@student.cis.edu.hk>
2023-03-07 12:31:29 +0800

    commit

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,38 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String f, String l, int a){
+        firstName = f;
+        lastName = l;
+        age = a;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PersonFactory {
 
@@ -9,14 +10,34 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
+        Collections.sort(allPersons); //sort list in alphabetical order first
 
+        int original_size = allPersons.size(); //note original size for line 23.
+
+        for(int i = 0; i < allPersons.size(); i++){ // compares last name of person object to each person object in arralyist.
+            String str1 = person.getLastName();
+            String str2 = allPersons.get(i).getLastName();
+            if(str2.compareTo(str1) >= 0) //if person object lastname is lexicographically less or equal than the person object from arraylist, insert person object there
+                allPersons.add(i, person);
+        }
+        if(original_size == allPersons.size()) // this means that the person object had not been added through the for loop above, and thus belongs at the end of the list.
+            allPersons.add(person);
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under18List = new ArrayList<>();
+        for (Person person: allPersons){
+            if(person.getAge() < 18)
+                under18List.add(person);
+        }
+        return under18List;
     }
 
     public boolean checkLastName(String lastName) {
-
+        for(Person person: allPersons){
+            if (person.getLastName().equals(lastName))
+                return true;
+        }
+        return false;
     }
 }
